307fd7f51b65f8efe518c050618a83a67f7a29bf
Emilly Wanjiku <emilly.wanjiku@student.moringaschool.com>
2022-11-29 08:40:08 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 62
+let num2 = 1;
+let multiply = num1*num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random()* 10)+ 1;
+let num3 = 37;
+let num4 = 11;
+let mod = num3 % num4;
+console.log(mod)
+
+let max = Math.max(0, 8, 20, 18, 15);
+console.log(max);
